3e087e81f02f4c832ae2257086520d3363b04ad4
Nicolas Fernandes <nicolaspfernandes@gmail.com>
2020-04-10 13:20:50 +0200

    Exporting LikeModule within AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { UserModule } from './user/user.module'
 import { PostModule } from './post/post.module'
+import { LikeModule } from './like/like.module'
 
 @Module({
   imports: [
     UserModule,
     PostModule,
+    LikeModule,
     TypeOrmModule.forRoot(),
     ConfigModule.forRoot({ envFilePath: '.env' }),
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' })
